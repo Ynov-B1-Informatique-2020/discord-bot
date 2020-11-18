6f53444696b2d26e2b0d4dea6446728cd62ba101
BGNAOUI <brahim.gnaoui@ynov.com>
2020-11-18 19:02:15 +0100

    link twitter

--- commands/index.js
+++ commands/index.js
@@ -10,4 +10,4 @@ exports.jsp = require('./jsp');
 exports.echo = require('./echo');
 exports.about = require('./about');
 exports.deleteafter = require('./deleteafter');
-exports.twitter = require('https://twitter.com/home');
+exports.twitter = require('./twitter');
