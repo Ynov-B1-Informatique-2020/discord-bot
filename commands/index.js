a1fa8ab4b3251e79cf24ff9d85b0b9310c8c4f56
Maxi-Mega <ixam.agem@gmail.com>
2020-11-19 01:01:40 +0100

    Added sha-1 & random commands

--- commands/index.js
+++ commands/index.js
@@ -1,6 +1,6 @@
 /*
  Notice: to add a command duplicate this line and replace ping by the name of your command
-         
+
  exports.ping = require('./ping');
          ^                 ^
 */
@@ -16,5 +16,5 @@ exports.antoine = require('./antoine');
 exports.twitter = require('./twitter');
 exports.weather = require('./weather');
 exports.chuck = require('./chuck');
-
-
+exports.sha1 = require('./sha1');
+exports.random = require('./random');
