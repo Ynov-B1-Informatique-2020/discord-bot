12e69f63174edcabf999b6d7c278205b5ee74aa4
ClementAddario <71002854+ClementAddario@users.noreply.github.com>
2020-11-23 17:59:11 +0100

    add hellothere (#24)

---
+++ commands/hellothere/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+    description: 'Return gif "Hello There"',
+    usage: '',
+    aliases: [],
+      execute(opts) {
+      opts.event.channel.send("https://images-ext-2.discordapp.net/external/BXptAbDaUAiIFwI0vm7qdwv8QC-y7LH177ey9G3IiLA/https/j.gifs.com/nx34Qp.gif?width=400&height=225");
+      },
+  };
