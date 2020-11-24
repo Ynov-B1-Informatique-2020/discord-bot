e532bea02c75afcc505f6fef1a3d42ee40b4e28e
Zao Soula <contact@zaosoula.fr>
2020-11-24 17:26:11 +0100

    Feat (help): Display detailed help section for a command

--- commands/help/index.js
+++ commands/help/index.js
@@ -1,14 +1,26 @@
 module.exports = {
 	description: 'Display the help section',
-  aliases: ['h', 'commands'],
+	aliases: ['h', 'commands'],
+	author: 'zaosoula',
 	execute(opts) {
-		let message = "Help\n\n";
-		opts.commands.forEach(i => {
-			message += `**${opts.Config.prefix + i.name}**`
-			if(i.aliases.length) message += ` (${i.aliases.join(', ')})`;
-			message += `: ${i.description}\n`;
-			if(i.usage) message += `\tUsage: *${opts.Config.prefix + i.name} ${i.usage}*\n`;
-		});
+		let message = "";
+		if(opts.argsRaw.length == 0) {
+			message += "Help\n\n"
+			opts.commands.forEach(i => {
+				message += `**${opts.Config.prefix + i.name}**`
+				if(i.aliases.length) message += ` (${i.aliases.join(', ')})`;
+				message += `: ${i.description}\n`;
+				if(i.usage) message += `\tUsage: *${opts.Config.prefix + i.name} ${i.usage}*\n`;
+			});
+		}else if(opts.argsRaw[0]) {
+			let commandName = opts.argsRaw[0].replace(opts.Config.prefix, '').toLowerCase();
+			let command = opts.commands.find((x)=> x.name == commandName || x.aliases.includes(commandName));
+			message += `Help for **${opts.Config.prefix + command.name}**\n`;
+			message += `**Aliases**: ${command.aliases.join(', ')}\n`;
+			message += `**Description**: ${command.description}\n`;
+			message += `**Usage**: ${opts.Config.prefix + command.name} ${command.usage}\n`;
+			message += `**Author**: ${command.author}\n`;
+		}
 
 		opts.event.channel.send(message);
 
