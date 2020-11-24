9aea4967730d7429e889cf0a05532ba8e7bb236d
Zao Soula <contact@zaosoula.fr>
2020-11-24 17:26:58 +0100

    Fix (echo): add author field

--- commands/echo/index.js
+++ commands/echo/index.js
@@ -2,6 +2,7 @@ module.exports = {
   description: 'Reply with the arguments',
   usage: 'string...',
   aliases: ['print', 'say'],
+  author: 'zaosoula',
 	execute(opts) {
     opts.event.channel.send(opts.argsRaw.join(' '));
 	},
